88f7f428cb5c8f9790e7fc44ce9163f3cb4fed09
Ajit1279 <Ajit1279.aiml@gmail.com>
2025-10-24 11:01:57 +0530

    amendments for fixing Cloud Function Deployment error

--- run-cloud.sh
+++ run-cloud.sh
@@ -12,6 +12,21 @@ sudo pip3 install -r requirements.txt
   TOPIC_NAME="neptune-activities"
   PROJECT_ID=$GOOGLE_CLOUD_PROJECT
 
+gcloud beta services identity create \
+  --service=pubsub.googleapis.com \
+  --project=$GOOGLE_CLOUD_PROJECT
+
+echo "pub-sub service account created successfully!"
+gcloud iam service-accounts list --filter="gcp-sa-pubsub"
+
+#Grant the token creator role to the Pub/Sub service account
+PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format="value(projectNumber)")
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
+  --member="serviceAccount:service-${PROJECT_NUMBER}@gcp-sa-pubsub.iam.gserviceaccount.com" \
+  --role="roles/iam.serviceAccountTokenCreator"
+echo "Granted Token Creator role to Pub/Sub service account!"
+
 echo "Deploying Cloud Function..."
 gcloud functions deploy pubsub_to_bigquery \
   --region=$REGION \
